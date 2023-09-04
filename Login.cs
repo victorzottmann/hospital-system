2e6abec6514ab69efa08051deeff8858f51d9a8a
Victor Zottmann <zottmann.victor@gmail.com>
2023-09-04 16:24:37 +1000

    add methods to display Login menu and validate credentials

---
+++ Login.cs
@@ -0,0 +1,93 @@
+﻿using HospitalSystem.Users;
+using System;
+using System.IO;
+
+namespace HospitalSystem
+{
+    public class Login
+    {
+        private string FilePath;
+
+        public Login(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+
+        public void DisplayMenu()
+        {
+            Console.Clear();
+
+            Menu loginMenu = new Menu();
+            loginMenu.Subtitle("Login");
+
+            Console.Write("ID: ");
+            string userId = Console.ReadLine()!;
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine()!;
+
+            ValidateCredentials(this.FilePath, userId, password);
+
+            Console.ReadKey();
+        }
+
+        public bool ValidateCredentials(string filepath, string userId, string password)
+        {
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    string[] lines = File.ReadAllLines(filepath);
+
+                    foreach (var line in lines)
+                    {
+                        string[] arr = line.Split(',');
+
+                        // trim strings to ensure there are no white spaces
+                        string targetUserId = arr[0].Trim();
+                        string targetPassword = arr[1].Trim();
+
+                        bool validUserId = userId == targetUserId;
+                        bool validPassword = password == targetPassword;
+
+                        if (validUserId && validPassword)
+                        {
+                            Console.WriteLine("Valid Credentials");
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"File not found: {filepath}");
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Invalid Credentials\n");
+
+            while (true)
+            {
+                Console.Write("Press 1 to try again, or 0 to exit: ");
+
+                string input = Console.ReadLine()!;
+
+                if (input == "1")
+                {
+                    this.DisplayMenu();
+                    break;
+                }
+                else if (input == "0")
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,19 +7,22 @@ namespace HospitalSystem
     {
         static void Main(string[] args)
         {
-            Address doctorJacobAddress = new Address("456 Albert Ave", "Sydney", "NSW");
-            Doctor doctorJacob = new Doctor("Jacob", "Hirsch", "j@hirsch.com", "234567890", doctorJacobAddress.ToString());
+            //Address doctorJacobAddress = new Address("456 Albert Ave", "Sydney", "NSW");
+            //Doctor doctorJacob = new Doctor("Jacob", "Hirsch", "j@hirsch.com", "234567890", doctorJacobAddress.ToString());
 
-            Address doctorSamAddress = new Address("789 Elizabth Street", "Sydney", "NSW");
-            Doctor doctorSam = new Doctor("Sam", "Smith", "d@smith.com", "345678901", doctorSamAddress.ToString());
+            //Address doctorSamAddress = new Address("789 Elizabth Street", "Sydney", "NSW");
+            //Doctor doctorSam = new Doctor("Sam", "Smith", "d@smith.com", "345678901", doctorSamAddress.ToString());
 
-            Address patientAddress = new Address("123 Victoria Ave", "Sydney", "NSW");
-            Patient patient = new Patient("Victor", "Zottmann", "v@z.com", "123456789", patientAddress.ToString());
+            //Address patientAddress = new Address("123 Victoria Ave", "Sydney", "NSW");
+            //Patient patient = new Patient("Victor", "Zottmann", "v@z.com", "123456789", patientAddress.ToString());
 
-            patient.AddAppointment(doctorJacob, "cold symptoms");
-            patient.AddAppointment(doctorSam, "regular checkup with doc");
+            //patient.AddAppointment(doctorJacob, "cold symptoms");
+            //patient.AddAppointment(doctorSam, "regular checkup with doc");
 
-            patient.DisplayMenu();
+            //patient.DisplayMenu();
+
+            Login login = new Login("LoginCredentials.txt");
+            login.DisplayMenu();
         }
     }
 }
