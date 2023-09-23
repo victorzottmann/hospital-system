b9f389d832f5d00090a97c6a25eed0a296e43e4c
Victor Zottmann <zottmann.victor@gmail.com>
2023-09-23 17:16:56 +1000

    add GetUserInputs

--- Users/Administrator.cs
+++ Users/Administrator.cs
@@ -56,51 +56,7 @@ namespace HospitalSystem
 
             Console.WriteLine($"Registering a new {userType.ToLower()} with the DOTNET Hospital Management System\n");
 
-            // organising the prompts in an array to loop through them
-            string[] prompts =
-            {
-                "First Name",
-                "Last Name",
-                "Email",
-                "Phone",
-                "Street Number",
-                "Street",
-                "City",
-                "State"
-            };
-
-            // creating an array of inputs to store them
-            string[] inputs = new string[prompts.Length];
-
-            // this loop prints the prompt in order and only proceeds to the next one if the input is not blank
-            for (int i = 0; i < prompts.Length; i++)
-            {
-                Console.Write($"{prompts[i]}: ");
-                string currentInput = Console.ReadLine()!;
-
-                // if the input is not blank
-                if (!string.IsNullOrWhiteSpace(currentInput))
-                {
-                    // the current input is stored in the inputs array
-                    inputs[i] = currentInput;
-                }
-                else
-                {
-                    // otherwise prompt the user to re-enter the value and repeat the same prompt
-                    Console.WriteLine($"The field cannot be empty!\n");
-                    i--;
-                }
-            }
-
-            // then if all goes well, store each input in their respective variable
-            string firstName = inputs[0].Trim();
-            string lastName = inputs[1].Trim();
-            string email = inputs[2].Trim();
-            string phone = inputs[3].Trim();
-            string streetNumber = inputs[4].Trim();
-            string street = inputs[5].Trim();
-            string city = inputs[6].Trim();
-            string state = inputs[7].Trim();
+            (string firstName, string lastName, string email, string phone, string streetNumber, string street, string city, string state) = GetUserInputs();
 
             Address address = new Address(streetNumber, street, city, state);
 
@@ -148,6 +104,56 @@ namespace HospitalSystem
         {
             string password = role == "Patient" ? "pat" : "doc";
             return password;
+        }
+
+        private (string firstName, string lastName, string email, string phone, string streetNumber, string street, string city, string state) GetUserInputs()
+        {
+            string[] prompts =
+            {
+                "First Name",
+                "Last Name",
+                "Email",
+                "Phone",
+                "Street Number",
+                "Street",
+                "City",
+                "State"
+            };
+
+            // creating an array of inputs to store them
+            string[] inputs = new string[prompts.Length];
+
+            // this loop prints the prompt in order and only proceeds to the next one if the input is not blank
+            for (int i = 0; i < prompts.Length; i++)
+            {
+                Console.Write($"{prompts[i]}: ");
+                string currentInput = Console.ReadLine()!.Trim();
+
+                // if the input is not blank
+                if (!string.IsNullOrWhiteSpace(currentInput))
+                {
+                    // the current input is stored in the inputs array
+                    inputs[i] = currentInput;
+                }
+                else
+                {
+                    // otherwise prompt the user to re-enter the value and repeat the same prompt
+                    Console.WriteLine($"The field cannot be empty!\n");
+                    i--;
+                }
+            }
+
+            // then if all goes well, store each input in their respective variable
+            string firstName = inputs[0];
+            string lastName = inputs[1];
+            string email = inputs[2];
+            string phone = inputs[3];
+            string streetNumber = inputs[4];
+            string street = inputs[5];
+            string city = inputs[6];
+            string state = inputs[7];
+
+            return (firstName, lastName, email, phone, streetNumber, street, city, state);
         }
 
         private void AddLoginCredential(string role, string filepath, string credentials)
